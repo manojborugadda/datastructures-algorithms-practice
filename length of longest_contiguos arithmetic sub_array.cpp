6f2b25fa6f67ed341f3c5c97db5afa25b75d8674
manojborugadda <borugadda.manoj@gmail.com>
2020-11-29 14:06:02 +0530

    Add files via upload

---
+++ length of longest_contiguos arithmetic sub_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+
+  int n;
+  cin>>n;
+  int arr[n];
+
+  for(int i = 0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  int curr = 2;
+  int ans = 2;
+  int j = 2;
+  int pd = arr[1]-arr[0];
+  while(j<n)
+  {
+      if(pd == arr[j]-arr[j-1])
+      {
+          curr++;
+      }
+      else
+        {
+        pd = arr[j]-arr[j-1];
+        curr = 2;
+        }
+        ans = max(ans,curr);
+    j++;
+  }
+  cout<<ans<<endl;
+  return 0;
+
+ }
+
