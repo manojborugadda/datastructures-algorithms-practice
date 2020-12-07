a9769d20cc5eff3b4a8f6a6a82f16e0c911f0638
manojborugadda <borugadda.manoj@gmail.com>
2020-12-07 18:04:37 +0530

    Add files via upload

---
+++ palindrome check basic.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+char arr[n+1];
+cin>>arr;
+bool check = 1;
+for(int i = 0;i<n;i++)
+{
+    if(arr[i]!= arr[n-1-i])
+    {
+        check = 0;
+        break;
+    }
+}
+
+if(check = false)
+{
+    cout<<"not found";
+}
+else
+{ cout<<"found";
+}
+return 0;
+}
+
+
