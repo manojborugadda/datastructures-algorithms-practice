dc885da4e80fe46d71af22a80a7dd2cd9a81b71b
manojborugadda <borugadda.manoj@gmail.com>
2020-12-09 22:48:02 +0530

    Add files via upload

---
+++ AMR15A codechef problem.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n,lucky = 0 , unlucky = 0;
+cin>>n;
+int arr[n];
+for(int i = 0;i<n;i++)
+{
+    cin>>arr[i];
+
+}
+
+for(int i = 0;i<n;i++)
+{
+    if(arr[i]%2 == 0 )
+      lucky++;
+    else
+        unlucky++;
+}
+if(lucky>unlucky)
+{
+    cout<<"READY FOR BATTLE"<<endl;
+}
+else
+{
+    cout<<"NOT READY"<<endl;
+}
+
+	return 0;
+}
