5a2f422cd3c742a1283afce30cd38f3e6f2b81ad
manojborugadda <borugadda.manoj@gmail.com>
2021-01-19 19:38:10 +0530

    Add files via upload

---
+++ Reverse of SinglyLinkedList.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+}Node;
+Node *head = NULL;
+
+void insert_at_Begin(int data)
+ {
+   Node *new_node = (Node*)(malloc(sizeof(Node)));
+    if(head == NULL)
+    {
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    head = new_node;
+    return;
+    }
+    new_node->data = data;
+    new_node->next = head;
+    new_node->prev = NULL;
+    head->prev = new_node;
+    head = new_node;
+ }
+
+ void reverse_list(Node **head)      /////////////reversing --------------
+ {
+     Node *prev = NULL;
+     Node *curr = *head;
+     Node *next = NULL;
+     while(curr!= NULL)
+     {
+         next = curr->next;
+         curr->next = prev;
+         prev = curr;
+         curr = next;
+     }
+     *head = prev;
+ }
+
+ void display()
+ {
+     Node *temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+ }
+
+ int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         int data;
+         for(int i = 0;i<n;i++)
+         {
+             cin>>data;
+              insert_at_Begin(data);
+         }
+         cout<<"inserting at the Beginning :";
+         display();
+         cout<<"\n";
+
+        cout<<"the linked list after reversing :";
+        reverse_list(&head);
+        display();
+        cout<<"\n";
+     }
+     return 0;
+ }
