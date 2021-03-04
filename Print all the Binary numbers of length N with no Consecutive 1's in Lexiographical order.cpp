91776034a99c0c8457f362eefa3c468aace8a368
Borugadda Manoj <borugadda.manoj@gmail.com>
2021-03-04 12:41:47 +0530

    Add files via upload

---
+++ Print all the Binary numbers of length N with no Consecutive 1's in Lexiographical order.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+void recurCombi(int arr[],string res,int i,int n,int k)
+{
+    if(k == 0)
+    {
+           cout<<res<<endl;
+    }
+    for(int j = i;j < n;j++)
+    {
+        recurCombi(arr,res+to_string(arr[j])+" ",j+1,n,k-1);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int k;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    cin>>k;
+    recurCombi(arr,"",0,n,k); // initially we take i =0 and empty string
+    return 0;
+}
