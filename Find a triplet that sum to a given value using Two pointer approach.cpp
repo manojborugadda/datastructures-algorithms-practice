c5f94c3b8afbef416e631fb6074c5181d3abe2e4
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-07-12 23:50:46 +0530

    Update Find a triplet that sum to a given value using Two pointer approach.cpp

--- Find a triplet that sum to a given value using Two pointer approach.cpp
+++ Find a triplet that sum to a given value using Two pointer approach.cpp
@@ -3,7 +3,7 @@
 /* Given an array and a value, find if there is a triplet in array whose sum is equal to the given value.
 If there is such a triplet present in array, then print the triplet and return true. Else return false.
 
-Input: array = {12, 3, 4, 1, 6, 9}, sum = 24;
+Input: array = {12, 3, 4, 1, 6, 9}, sum = 24; 
 Output: 12, 3, 9
  */
 
