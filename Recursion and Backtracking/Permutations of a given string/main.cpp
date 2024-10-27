d297446f0bc30a741d2f3258232abc165850e53f
Manoj Borugadda <borugadda.manoj@gmail.com>
2024-10-27 14:08:23 +0530

    Create main.cpp

---
+++ Recursion and Backtracking/Permutations of a given string/main.cpp
@@ -0,0 +1,7 @@
+// Given a string s. The task is to return a vector of string of all unique permutations of the given string, s that may contain dulplicates in lexicographically sorted order. 
+
+// Examples:
+
+// Input: ABC
+// Output: [ABC, ACB, BAC, BCA, CAB, CBA]
+// Explanation: Given string ABC has permutations in 6 forms as ABC, ACB, BAC, BCA, CAB and CBA .
